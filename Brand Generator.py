ccbc931b58416d99ab5e1d215f57884a6b5b729e
saratkrishna1995 <62541115+saratkrishna1995@users.noreply.github.com>
2025-01-25 19:03:53 +0530

    Create Brand Generator.py

---
+++ Brand Generator.py
@@ -0,0 +1,4 @@
+print("Hey there! Welcome to Brand name Generator")
+City=input("What is the city you grew up in? \n")
+Pet=input("What is your pet name? \n")
+print("Suggestion for the brand name could be: "+ City + Pet)
